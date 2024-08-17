c7d01bf7b4c2fb13f09d02853554d63f64570297
jmininel <ju.mininel@hotmail.com>
2024-08-17 20:48:49 -0300

    feat: update BarbershopPage

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { SearchIcon } from "lucide-react"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearckOption } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
@@ -24,56 +25,34 @@ const Home = async () => {
         <h2 className="text-xl font-bold">Olá, Juliana</h2>
         <p>Segunda-feira 05 Agosto</p>
 
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca" />
-          <Button>
-            <SearchIcon />
-          </Button>
-        </div>
-
-        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="Cabelo" />
-            Cabelo
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="Barba" />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/acabamento.svg"
-              width={16}
-              height={16}
-              alt="acabamento"
+        <div className="mt-4 flex justify-center">
+          <div className="flex w-full items-center gap-2 rounded-lg sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5">
+            <Input
+              type="text"
+              placeholder="Faça sua busca"
+              className="w-full rounded-l-lg p-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="Cabelo" />
-            Pezinho
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="Barba" />
-            Sobrancelha
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="Cabelo" />
-            Bigode
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="Barba" />
-            Barba
-          </Button>
+            <Button className="rounded-r-lg bg-indigo-500 p-2 text-white hover:bg-indigo-700 focus:bg-indigo-700 focus:outline-none">
+              <SearchIcon />
+            </Button>
+          </div>
         </div>
 
-        <div className="relative mt-6 h-[150px] w-full">
+        <div className="mt-6 flex gap-3 overflow-x-scroll opacity-100 transition-opacity duration-1000 ease-in-out md:pointer-events-none md:hidden md:opacity-0 [&::-webkit-scrollbar]:hidden">
+          {quickSearckOption.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full md:hidden">
           <Image
             src="/banner-01.png"
             alt="Agende com os melhores na Barber-Shop"
@@ -82,31 +61,7 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barber Shop</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
