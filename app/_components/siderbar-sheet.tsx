b6b6980d1bec1033f4f59e81ecf7c32624dfddbd
jmininel <ju.mininel@hotmail.com>
2024-08-23 14:27:14 -0300

    chore: add next auth setup

--- app/_components/siderbar-sheet.tsx
+++ app/_components/siderbar-sheet.tsx
@@ -1,10 +1,17 @@
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
 import { Button } from "./ui/button"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
-import Link from "next/link"
 import { quickSearckOption } from "../_constants/search"
+import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SiderbarSheet = () => {
   return (
@@ -13,15 +20,41 @@ const SiderbarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <LogInIcon size="icon" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                width={18}
+                height={18}
+                alt="Fazer login com a conta do Google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/*    <Avatar>
           <AvatarImage src="https://www.stryx.com/cdn/shop/articles/man-looking-attractive.jpg?v=1666662774" />
         </Avatar>
 
         <div>
           <p className="font-bold">Tom Hask</p>
           <p className="text-xs">tomhask@hotmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
